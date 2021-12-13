ab419106fec68af2cf31c9bfbd44aa58c1702409
BortKiD <30574689+BortKiD@users.noreply.github.com>
2021-12-14 02:42:30 +0300

    Main project files

---
+++ Tetris/app/src/main/java/com/example/tetris/GameActivity.kt
@@ -0,0 +1,173 @@
+package com.example.tetris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.GridLayout
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import com.example.tetris.databinding.ActivityBoardBinding
+import kotlin.random.Random
+
+class GameActivity : AppCompatActivity() {
+
+    val row = 36
+    val column = 26
+    var start = true
+    var speed: Long = 300
+
+    var top = 0
+    var bottom = 0
+    var right = 0
+    var left = 0
+
+    var isFullLine = 0
+
+    lateinit var binding: ActivityBoardBinding
+
+    var piece = Piece(1)
+
+    var board = Array(row) {
+        Array(column){0}
+    }
+
+    var boardView = Array(row) {
+        arrayOfNulls<ImageView>(column)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_board)
+
+        binding.moveLeftButton.setOnClickListener {
+            if(piece.isCorrectMoveLeft(board)) {
+                piece.moveLeft()
+            }
+        }
+
+        binding.moveRightButton.setOnClickListener {
+            if(piece.isCorrectMoveRight(board)) {
+                piece.moveRight()
+            }
+        }
+
+        binding.moveDownButton.setOnClickListener {
+            if(piece.isCorrectMoveDown(board)){
+                piece.moveDown()
+            }
+        }
+
+        binding.rotateButton.setOnClickListener {
+            piece.isCorrectRotate(board)
+        }
+
+        var gridBoard = findViewById<GridLayout>(R.id.gridBoard)
+        gridBoard.rowCount = row
+        gridBoard.columnCount = column
+
+        val inflater = LayoutInflater.from(this)
+
+        for (i in 0 until row) {
+            for (j in 0 until column) {
+                boardView[i][j] = inflater.inflate(R.layout.inflate_image_view, gridBoard, false) as ImageView
+                gridBoard.addView(boardView[i][j])
+            }
+        }
+
+        processGameEvents()
+    }
+
+    fun getBorder(piece: Piece) {
+        for (i in 0..3) for (j in 0..3) if (piece.piece[i][j] == 1) {
+            bottom = i
+            break
+        }
+        for (i in 3 downTo 0) for (j in 0..3) if (piece.piece[i][j] == 1) {
+            top = i
+            break
+        }
+        for (j in 0..3) for (i in 0..3) if (piece.piece[i][j] == 1) {
+            right = j
+            break
+        }
+        for (j in 3 downTo 0) for (i in 0..3) if (piece.piece[i][j] == 1) {
+            left = j
+            break
+        }
+    }
+
+    fun processGameEvents() {
+        Thread {
+            while (start) {
+                Thread.sleep(speed)
+                runOnUiThread {
+                    getBorder(piece)
+
+                    for (i in 0 until row) {
+                        for (j in 0 until column) {
+                                if ((i == 0 || i == row - 1) || (j == 0 || j == column - 1)) {
+                                    boardView[i][j]!!.setImageResource(R.drawable.stable)
+                                    board[i][j] = 2
+                                } else if(board[i][j] == 1) {
+                                    boardView[i][j]!!.setImageResource(R.drawable.falling)
+                                } else {
+                                    boardView[i][j]!!.setImageResource(R.drawable.back)
+                                }
+                        }
+                    }
+
+                    if (piece.isCorrectMoveDown(board)) {
+                        piece.moveDown()
+                    } else {
+                        board = piece.drawPiece(board)
+                        piece = Piece(Random.nextInt(1, 8))
+                    }
+
+                    // Broken plan A
+                    for (i in 0 until piece.pieceRow) {
+                        for (j in 0 until piece.pieceCol) {
+                            if (piece.py + bottom == row - 1) {
+                                start = false
+                                break
+                            }
+                            if (piece.piece[i][j] == 1) {
+                                boardView[piece.px + i][piece.py + j]!!.setImageResource(R.drawable.falling)
+                            }
+                        }
+                    }
+
+                    var i = row - 2
+                    isFullLine = 0
+                    while (i >= 2) {
+                        for (j in 1 until column - 1)
+                            if (board[i][j] == 0) {
+                                isFullLine = 0
+                                i--
+                                break
+                            }
+                        if (isFullLine == 1) {
+                            for (k in i downTo 2)
+                                for (j in 1 until column - 1)
+                                    board[k][j] = board[k - 1][j]
+                        }
+                    }
+/* Plan B
+                    try {
+                        for (i in 0 until piece.pieceRow) {
+                            for (j in 0 until piece.pieceCol) {
+                                if (piece.piece[i][j] == 1) {
+                                    boardView[piece.px + i][piece.py + j]!!.setImageResource(R.drawable.falling)
+                                }
+                            }
+                        }
+                    } catch (e: ArrayIndexOutOfBoundsException ) {
+                        start = false
+                    }
+
+ */
+                }
+            }
+        }.start()
+    }
+}
---
+++ Tetris/app/src/main/java/com/example/tetris/Piece.kt
@@ -0,0 +1,192 @@
+package com.example.tetris
+
+class Piece {
+
+    var pieceRow = 4
+    var pieceCol = 4
+
+    var px = 1
+    var py = 11
+
+    var type: Int = 0
+
+    constructor(type: Int){
+        this.type = type
+        createPiece(type)
+    }
+
+    var piece = Array(pieceRow) {
+        Array(pieceCol){0}
+    }
+
+
+
+    fun createPiece(type:Int){
+
+        when(type){
+            //I-block
+            1 -> {
+                this.piece[1][0] = 1
+                this.piece[1][1] = 1
+                this.piece[1][2] = 1
+                this.piece[1][3] = 1
+            }
+            //O-block
+            2 -> {
+                this.piece[1][1] = 1
+                this.piece[1][2] = 1
+                this.piece[2][1] = 1
+                this.piece[2][2] = 1
+            }
+            //T-block
+            3 -> {
+                this.piece[1][0] = 1
+                this.piece[1][1] = 1
+                this.piece[1][2] = 1
+                this.piece[2][1] = 1
+            }
+            //J-block
+            4 -> {
+                this.piece[1][0] = 1
+                this.piece[2][0] = 1
+                this.piece[2][1] = 1
+                this.piece[2][2] = 1
+            }
+            //L-block
+            5 -> {
+                this.piece[2][0] = 1
+                this.piece[2][1] = 1
+                this.piece[2][2] = 1
+                this.piece[1][2] = 1
+            }
+            //S-block
+            6 -> {
+                this.piece[2][0] = 1
+                this.piece[2][1] = 1
+                this.piece[1][1] = 1
+                this.piece[1][2] = 1
+            }
+            //Z-block
+            7 -> {
+                this.piece[1][0] = 1
+                this.piece[1][1] = 1
+                this.piece[2][1] = 1
+                this.piece[2][2] = 1
+            }
+        }
+
+    }
+
+    fun isCorrectMoveRight(board: Array<Array<Int>>):Boolean{
+        for(i in 0 until pieceRow){
+            for(j in 0 until pieceCol){
+                if(this.piece[i][j] == 1){
+                    if(board[this.px+i][this.py+j+1] != 0)
+                        return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    fun moveRight(){
+        py++
+    }
+
+    fun isCorrectMoveLeft(board: Array<Array<Int>>):Boolean{
+        for(i in 0 until pieceRow){
+            for(j in 0 until pieceCol){
+                if(this.piece[i][j] == 1){
+                    if(board[this.px+i][this.py+j-1] != 0)
+                        return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    fun moveLeft(){
+        py--
+    }
+
+    fun isCorrectMoveDown(board:Array<Array<Int>>): Boolean {
+
+        for (i in 0 until pieceRow) {
+            for (j in 0 until pieceCol) {
+                if (this.piece[i][j] == 1) {
+                    if (board[this.px+i+1][this.py+j] != 0) {
+                        return false
+                    }
+                }
+            }
+        }
+
+        return true
+    }
+
+    fun moveDown(){
+      this.px++
+    }
+
+    fun isCollide(board:Array<Array<Int>>):Boolean{
+
+        for (i in 0 until pieceRow) {
+            for (j in 0 until pieceCol) {
+                if (this.piece[i][j] == 1) {
+                    if (isCorrectMoveRight(board) && isCorrectMoveLeft(board) && isCorrectMoveDown(board)) {
+                        if (board[this.px+i+2][this.py+j] != 0
+                                || board[this.px+i][this.py+j-2] != 0
+                                || board[this.px+i][this.py+j+2] != 0) {
+                            return false
+                        }
+                    } else {
+                        return false
+                    }
+                }
+            }
+        }
+        return true
+    }
+
+    fun isCorrectRotate(board:Array<Array<Int>>) {
+
+        if(this.type != 1){
+            if(isCorrectMoveRight(board) && isCorrectMoveLeft(board) && isCorrectMoveDown(board)){
+                rotatePiece()
+            }
+        } else {
+            if(isCollide(board)){
+                rotatePiece()
+            }
+        }
+    }
+
+    fun rotatePiece() {
+
+        var temp = Array(pieceRow) {
+            Array(pieceCol) { 0 }
+        }
+
+        for (i in 0 until pieceRow) {
+            for ((aux, j) in (pieceCol - 1 downTo 0).withIndex()) {
+                temp[i][aux] = piece[j][i]
+            }
+        }
+
+        piece = temp
+    }
+
+    fun drawPiece(board:Array<Array<Int>>):Array<Array<Int>> {
+        for(i in 0 until pieceRow) {
+            for (j in 0 until pieceCol) {
+                if(this.piece[i][j] == 1) {
+                    board[this.px+i][this.py+j] = 1
+                }
+            }
+        }
+
+        return board
+    }
+}
